6f0f2050a9a41eb2a50ad3aca321530b6b28f0e8
yiannisha <hadjiyiannis.3@gmail.com>
2024-09-12 09:52:23 +0300

    Add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use pyo3::prelude::*;
 use regex::Regex;
 use std::collections::HashMap;
 
+#[derive(Debug)]
 #[pyclass]
 pub struct ParsedDocstring {
     #[pyo3(get, set)]
@@ -52,4 +53,133 @@ fn llm_tool(m: &Bound<'_, PyModule>) -> PyResult<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_empty_string() {
+        let docstring = "";
+        let parsed_docstring = parse_docstring(docstring);
+        assert_eq!(parsed_docstring.description, "");
+        assert_eq!(parsed_docstring.returns, "");
+        assert_eq!(parsed_docstring.params, HashMap::new());
+    }
+
+    #[test]
+    fn test_normal_string() {
+        let docstring = "
+        This is a description
+        :param x: This is the description of x
+        :param y: This is the description of y
+        :return: This is the return description
+        ";
+        let parsed_docstring = parse_docstring(docstring);
+        assert_eq!(parsed_docstring.description, String::from("This is a description"));
+        assert_eq!(parsed_docstring.returns, String::from("This is the return description"));
+        assert_eq!(parsed_docstring.params, HashMap::from([(String::from("x"), String::from("This is the description of x")), (String::from("y"), String::from("This is the description of y"))]));
+    }
+
+    #[test]
+    fn test_no_description() {
+        let docstring = "
+        :param x: This is the description of x
+        :param y: This is the description of y
+        :return: This is the return description
+        ";
+        let parsed_docstring = parse_docstring(docstring);
+        assert_eq!(parsed_docstring.description, String::from(""));
+        assert_eq!(parsed_docstring.returns, String::from("This is the return description"));
+        assert_eq!(parsed_docstring.params, HashMap::from([(String::from("x"), String::from("This is the description of x")), (String::from("y"), String::from("This is the description of y"))]));
+    }
+    
+    #[test]
+    fn test_no_params() {
+        let docstring = "
+        This is a description
+        :return: This is the return description
+        ";
+        let parsed_docstring = parse_docstring(docstring);
+        assert_eq!(parsed_docstring.description, String::from("This is a description"));
+        assert_eq!(parsed_docstring.returns, String::from("This is the return description"));
+        assert_eq!(parsed_docstring.params, HashMap::new());
+    }
+
+    #[test]
+    fn test_no_return() {
+        let docstring = "
+        This is a description
+        :param x: This is the description of x
+        :param y: This is the description of y
+        ";
+        let parsed_docstring = parse_docstring(docstring);
+        assert_eq!(parsed_docstring.description, String::from("This is a description"));
+        assert_eq!(parsed_docstring.returns, String::from(""));
+        assert_eq!(parsed_docstring.params, HashMap::from([(String::from("x"), String::from("This is the description of x")), (String::from("y"), String::from("This is the description of y"))]));
+    }
+
+    #[test]
+    fn test_no_param_description() {
+        let docstring = "
+        This is a description
+        :param x:
+        :param y:
+        :param z: This is the description of z
+        :return: This is the return description
+        ";
+        let parsed_docstring = parse_docstring(docstring);
+        assert_eq!(parsed_docstring.description, String::from("This is a description"));
+        assert_eq!(parsed_docstring.returns, String::from("This is the return description"));
+        assert_eq!(parsed_docstring.params, HashMap::from([(String::from("x"), String::from("")), (String::from("y"), String::from("")), (String::from("z"), String::from("This is the description of z"))]));
+    }
+
+    #[test]
+    fn test_no_return_description() {
+        let docstring = "
+        This is a description
+        :param x: This is the description of x
+        :param y: This is the description of y
+        :return:
+        ";
+        let parsed_docstring = parse_docstring(docstring);
+        assert_eq!(parsed_docstring.description, String::from("This is a description"));
+        assert_eq!(parsed_docstring.returns, String::from(""));
+        assert_eq!(parsed_docstring.params, HashMap::from([(String::from("x"), String::from("This is the description of x")), (String::from("y"), String::from("This is the description of y"))]));
+    }
+
+    #[test]
+    fn test_no_param_name() {
+        let docstring = "
+        This is a description
+        :param : This is the description of x
+        :param y: This is the description of y
+        :return: This is the return description
+        ";
+        let parsed_docstring = parse_docstring(docstring);
+        assert_eq!(parsed_docstring.description, String::from("This is a description"));
+        assert_eq!(parsed_docstring.returns, String::from("This is the return description"));
+        assert_eq!(parsed_docstring.params, HashMap::from([(String::from(""), String::from("This is the description of x")), (String::from("y"), String::from("This is the description of y"))]));
+    }
+
+    #[test]
+    fn test_mispelled_return() {
+        let docstring = "
+        This is a description
+        :param x: this is the description of x
+        :retrun: This is the return description
+        ";
+        let parse_docstring = parse_docstring(docstring);
+        assert_eq!(parse_docstring.returns, String::from(""));
+    }
+
+    #[test]
+    fn test_weird_colons() {
+        let docstring = "
+        This is a description
+        :param x: This is the d:escriptio:n of x
+        :param y: This is :the description of y
+        :return: This is the return description
+        ";
+        let parsed_docstring = parse_docstring(docstring);
+        assert_eq!(parsed_docstring.description, String::from("This is a description"));
+        assert_eq!(parsed_docstring.returns, String::from("This is the return description"));
+        assert_eq!(parsed_docstring.params, HashMap::from([(String::from("x"), String::from("This is the d")), (String::from("y"), String::from("This is"))]));
+    }
 }
